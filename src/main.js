e1e3abed87123e01f24dd8fb13e85f2e816bd5aa
Ryan Cheung <ryanc@livefyre.com>
2014-09-15 11:24:36 -0700

    Update exported module properties for GalleryView

--- src/main.js
+++ src/main.js
@@ -5,6 +5,5 @@ var CarouselAnimator = require('./animators/carousel-animator');
 var CoverflowAnimator = require('./animators/coverflow-animator');
 
 exports = module.exports = GalleryView;
-exports.GalleryView = GalleryView;
 exports.CarouselAnimator = CarouselAnimator;
 exports.CoverflowAnimator = CoverflowAnimator;
